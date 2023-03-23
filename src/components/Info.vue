ae3cb43bda5fe79c9011441eb4df294a4f13214c
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2023-03-23 15:24:05 +0200

    updaate

---
+++ src/components/Info.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+     <p>Estou trabalhando no momento</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+          <li>React</li>
+          <li>Vue JS</li>
+          <li>Javascript</li>
+        </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Info',
+}
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
---
+++ src/components/Pessoa.vue
@@ -0,0 +1,21 @@
+<template>
+  <Info/>
+</template>
+
+<script>
+
+import Pessoa from './Info.vue'
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Pessoa',
+  component: Pessoa,
+
+}
+</script>
+
+
+
+<style>
+
+</style>
