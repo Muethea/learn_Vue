bb04d1e9fc59a3fc99562e0ba80af424092b2d4c
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2023-03-23 15:45:57 +0200

    diretivas

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-     <p>Estou trabalhando no momento</p>
+     <p v-if="trabalhando">Estou trabalhando no momento</p>
+     <p v-else>Em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
           <li>React</li>
           <li>Vue JS</li>
           <li>Javascript</li>
         </ul>
+
+        <p v-show="mostraremail">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
+
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Info',
+
+    data(){
+      return {
+        trabalhando:true, 
+        mostraremail:false,
+          email:"email@email.com"
+      }
+    }
 }
 </script>
 
