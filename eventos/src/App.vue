5ed89106f6ff83e62bad9a17bc7e2bc231f05146
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2023-03-24 10:44:05 +0200

    Eventos

---
+++ eventos/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <EventosView/>
+</div>
+</template>
+
+<script>
+
+import EventosView from './components/EventosView.vue';
+
+
+export default {
+    name: "App",
+    components: { EventosView }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ eventos/src/components/EventosView.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Eventos</h1>
+
+  <form action="" @submit.prevent="enviar">
+  <p><input type="text" placeholder="nome" v-model="name"></p>
+  <p><input type="email" placeholder="email" v-model="email"></p>
+  <p><input type="password" placeholder="password" v-model="password"></p>
+
+  <button >Enviar</button>
+
+  <h1>{{ informacao }}</h1>
+  </form>
+</template>
+
+<script>
+export default {
+
+  data(){
+    return{
+      name:'',
+      email:'',
+      password:'',
+      informacao:'',
+    }
+  },
+
+  methods:{
+    enviar(){
+
+      this.informacao = this.name+" "+this.email+" "+this.password;
+
+      
+    }
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
